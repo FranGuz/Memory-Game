5f6f4c94419df3793f9620acc34b1cb0ff69c75d
FranGuz <guzmanf92@gmail.com>
2017-11-08 16:59:09 -0500

    recent

--- script.js
+++ script.js
@@ -2,37 +2,44 @@ $(document).ready(function(){
 	
 	var classes = ["red", "red", "blue", "blue", "green", "green", "yellow", "yellow"];
 	var upsideDown = $(".upsideDown");
-    var upsideDownLength = upsideDown.length;
+	var upsideDownLength = upsideDown.length;
+	var cardOne = null;
+	var cardTwo = null;
 
+	$("#start").on("click",function() {
+		$(".container").fadeIn();
+	});
+	
 upsideDown.each(function(){
 	var g = Math.floor(Math.random()*classes.length);
 	$(this).addClass(classes[g]);
 	classes.splice(g, 1);
 });
 	
-upsideDown.on("click", function(){
-	var cardOne = $(this);
-	cardOne.off("click");
+$(".upsideDown").on("click", function(){
+	$(this).removeClass("upsideDown");
+	cardOne = $(this);
+	console.log(cardOne)
+	});
 
-	upsideDown.on("click", function(){
-		var cardTwo = $(this);	  
-		
+if ($("div").hasClass("upsideDown")){
+	$(".upsideDown").on("click", function(){
+	cardTwo = $(this);	 
+	cardTwo.removeClass("upsideDown");
+	console.log(cardTwo);
+	console.log(cardOne);
+	
 
-		console.log(cardTwo);
-		console.log(cardOne);
-		if (cardTwo.attr("class") === cardOne.attr("class")) {
-			console.log("we won");
-	cardOne.css("display","none");
-	cardTwo.css("display","none");
-		
-		
-} else {
-			console.log("we lost");
-		
-  }
+	if (cardTwo.attr("class") === cardOne.attr("class")) {
+		console.log("we won");
+		cardOne.fadeOut(500);
+		cardTwo.fadeOut(500);
+
+
+	} else {
+			console.log("we lost");	
+    	}
 		});
-});
-	
+	}
 
-	
 });
