601579ee58e9b568ce37da9dc1b2371cb448e3e3
FranGuz <guzmanf92@gmail.com>
2017-11-08 13:45:29 -0500

    2 by 2

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ $(document).ready(function(){
     var upsideDownLength = upsideDown.length;
 
 
-for (i = 0; i < upsideDownLength; i++) {
-		var g = Math.floor(Math.random()*classes.length);
-		upsideDown.addClass(classes[g]);
-		classes.splice(g, 1);
-		upsideDown.splice(g, 1);
-		console.log(upsideDown);
-	}
+// for (i = 0; i < upsideDownLength; i++) {
+// 		var g = Math.floor(Math.random()*classes.length);
+		
+// 		upsideDown.addClass(classes[g]);
+// 		classes.splice(g, 1);
+// 		upsideDown.splice(g, 1);
+// 		console.log(upsideDown);
+// 	}
+
+upsideDown.each(function(){
+	var g = Math.floor(Math.random()*classes.length);
+	$(this).addClass(classes[g]);
+	classes.splice(g, 1);
+});
 	
 
 	
