9f52e52357c0cbdc85b7244f38d7addee5cef25b
Mary Clare Harrington <maryclareharrington@Marys-MacBook-Air.local>
2017-11-08 11:03:05 -0500

    mc first push

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+	var classes = ["red", "red", "blue", "blue", "green", "green", "yellow", "yellow"];
+	
+	var upsideDown = $(".upsideDown");
+//	console.log(upsideDown);
+	
+
+	for (i = 0; i < upsideDown.length; i++) {
+		upsideDown.addClass("red");
+	}
+	
+
+	
+});
