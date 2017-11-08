e4cf7fb2bb6a7b3d9128ff908c7ce4f3cb945df7
sherif fayez <sherifbaraya@gmail.com>
2017-11-08 11:58:25 -0500

    last update

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
 	
 	var classes = ["red", "red", "blue", "blue", "green", "green", "yellow", "yellow"];
-	
 	var upsideDown = $(".upsideDown");
-//	console.log(upsideDown);
-	
+    var upsideDownLength = upsideDown.length;
 
-	for (i = 0; i < upsideDown.length; i++) {
+
+for (i = 0; i < upsideDownLength; i++) {
 		var g = Math.floor(Math.random()*classes.length);
-		console.log(classes[g]);
+		upsideDown.addClass(classes[g]);
 		classes.splice(g, 1);
+		upsideDown.splice(g, 1);
+		console.log(upsideDown);
 	}
 	
 
