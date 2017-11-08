776c8f509612b5be70a6f99ff42d3c25c20ce6c1
FranGuz <guzmanf92@gmail.com>
2017-11-08 11:28:24 -0500

    rando

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 	
 
 	for (i = 0; i < upsideDown.length; i++) {
-		upsideDown.addClass("red");
+		var g = Math.floor(Math.random()*classes.length);
+		console.log(g);
 	}
 	
 
