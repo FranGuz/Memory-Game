a00396f6c1f0dd07ac9669858c3053ed40044bc6
sherif fayez <sherifbaraya@gmail.com>
2017-11-10 10:39:29 -0500

    delay fixed again

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ $(document).ready(function(){
 
 		if (cardTwo.attr("class") === cardOne.attr("class")) {
 			console.log("we won");
-			cardOne.delay(200).fadeOut(250);
-			cardTwo.delay(200).fadeOut(250);
+			cardOne.delay(3000).fadeOut(250);
+			cardTwo.delay(3000).fadeOut(250);
 			cardOne = null;
 			cardTwo = null;
 		} else {
